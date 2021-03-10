56d8689c961b58a4648b37c8b0770d2559f9e82a
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-09 22:52:24 -0600

    Added notes/answers to number 1 and 2 of the module project in regards to Lambda's U.P.E.R. framework

---
+++ module_project.py
@@ -0,0 +1,22 @@
+
+# CodeSignal assignment for module 1.1 #
+
+"""
+Exercise 1: What are the steps in Lambda's U.P.E.R process?
+
+1. Understand
+2. Plan
+3. Execute
+4. Reflect
+"""
+
+"""
+Exercise 2: What to do in the "Plan" step of UPER? (multiple choice question)
+
+Create an actionable/easy to comprehend method of attacking the problem, often
+using pseudocode to make sense of the problem
+
+Tips:
+    - identify issues or challenges and determine whether you need to go back 
+    in order to the understand the problem and/or ask more questions in "U" step
+"""
