5045a7ff69c52f245c3dfb476d35a9242d9e0e3f
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-09 23:22:21 -0600

    Added an updated solution to exercise 3 that is more concise and simpler

--- module_project.py
+++ module_project.py
@@ -10,6 +10,8 @@ Exercise 1: What are the steps in Lambda's U.P.E.R process?
 4. Reflect
 """
 
+
+
 """
 Exercise 2: What to do in the "Plan" step of UPER? (multiple choice question)
 
@@ -20,6 +22,7 @@ Tips:
     - identify issues or challenges and determine whether you need to go back 
     in order to the understand the problem and/or ask more questions in "U" step
 """
+
 
 
 """
@@ -44,29 +47,42 @@ Any string that contains spaces or is empty should return False.
 """
 
 def csAlphanumericRestriction(input_str):
-    # Can use Python built in api to check if numeric
-    # i.e. will return true if all numbers in str
-    if input_str.isnumeric():
+    # Can use Python built in api to check if numeric or alpha
+    # i.e. will return true if all numbers or all alpha in str
+    if input_str.isnumeric() or input_str.isalpha():
         return True
-    
-    # If not then iterate through characters and ensure all are letters
-    # can use 'AND' keyword combining if statements
-    # if char_in_str.isnumeric() == FALSE and char_in_str.isalnum()
-    
-    # Using this process will ensure that the characters are alphanumeric,
-    # but will return False if a number is included
-    for char in input_str:
-        if char.isnumeric() == False and char.isalnum():
-            continue
-        else:
-            return False
-    
-    return True
+    else:
+        return False
+
+print(csAlphanumericRestriction("123"))
+print(csAlphanumericRestriction("abc"))
+print(csAlphanumericRestriction("12c3"))
+
+# Original solution below -> was unaware of the "isalpha()" method on str
+    # def csAlphanumericRestriction(input_str):
+    #     # Can use Python built in api to check if numeric
+    #     # i.e. will return true if all numbers in str
+    #     if input_str.isnumeric():
+    #         return True
+        
+    #     # If not then iterate through characters and ensure all are letters
+    #     # can use 'AND' keyword combining if statements
+    #     # if char_in_str.isnumeric() == FALSE and char_in_str.isalnum()
+        
+    #     # Using this process will ensure that the characters are alphanumeric,
+    #     # but will return False if a number is included
+    #     for char in input_str:
+    #         if char.isnumeric() == False and char.isalnum():
+    #             continue
+    #         else:
+    #             return False
+        
+    #     return True
 
 
 
 """
-Exercise 3: Write a function that takes a string as input and returns that string in reverse order, with the opposite casing for each character within the string.
+Exercise 4: Write a function that takes a string as input and returns that string in reverse order, with the opposite casing for each character within the string.
 
 Examples:
 
