d6d96724678d0fa683812dd993e959d228c64d77
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-09 23:12:12 -0600

    Added exercise 3 coding question

--- module_project.py
+++ module_project.py
@@ -62,3 +62,23 @@ def csAlphanumericRestriction(input_str):
             return False
     
     return True
+
+
+
+"""
+Exercise 3: Write a function that takes a string as input and returns that string in reverse order, with the opposite casing for each character within the string.
+
+Examples:
+
+csOppositeReverse("Hello World") ➞ "DLROw OLLEh"
+csOppositeReverse("ReVeRsE") ➞ "eSrEvEr"
+csOppositeReverse("Radar") ➞ "RADAr"
+Notes:
+
+The input string will only contain alpha characters.
+[execution time limit] 4 seconds (py3)
+
+[input] string txt
+
+[output] string
+"""
