fe9379d5c690e8f62623832fb0e5c207fa8ca99e
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-11 21:04:05 -0600

    Added notes for the UPER framework of problem solving

--- 1.2/lecture_notes.py
+++ 1.2/lecture_notes.py
@@ -1,2 +1,38 @@
-# 
 
+"""
+# Sprint 1 Module 2 - Problem Solving
+
+The U.P.E.R. Framework:
+
+(U)nderstand
+    - know what youre being asked to do
+    - create 3-5 test-cases, you can actually discover the optimal algorithm in doing so
+        - tests should not be too easy or too hard
+
+(P)lan
+    - takes the most time for this step
+    - you know the problem, whats the game plan?
+    - address each of the cases you developed in the Understand step
+    - what patterns have you seen before that you can apply?
+    - what techniques/data structures can I use?
+    - start with brute force if no immediate optimal solution
+    - rough layout of code
+    - what functions do I need?
+    - DO NOT PROCEED TO NEXT STEP if you don't know what you'll be coding
+
+(E)xecute
+    - convert your pseudocode into actual code
+    - code your algorithm
+    - if you plan properly this should be the fastest step yet
+    - pay attention to any bugs
+
+(R)eflect
+    - correct any bugs
+        - make a note of the bug but don't immediately address
+        - make sure to take time to and plan how to fix
+            - DO NOT do anything rash and regress your code
+    - go through line by line and explain the solution
+    - test your code with your test cases from step 1
+    - state your runtime and space complexity if you havent addressed in plan step
+
+"""
