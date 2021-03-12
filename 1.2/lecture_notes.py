a5e70010082c92604050796415183e9bd6546385
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-11 22:33:45 -0600

    Added prompt for exercise 1

--- 1.2/lecture_notes.py
+++ 1.2/lecture_notes.py
@@ -36,3 +36,20 @@ The U.P.E.R. Framework:
     - state your runtime and space complexity if you havent addressed in plan step
 
 """
+
+
+"""
+Exercise 1: https://leetcode.com/problems/number-of-good-pairs/
+
+Given an array of integers nums.
+
+A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+
+Return the number of good pairs.
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+"""
