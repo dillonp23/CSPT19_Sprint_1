16cc0d6667253775a46de6d3aec716807d9b58c0
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-12 23:53:08 -0600

    Added prompt for exercise 2

--- 1.2/module_assignment.py
+++ 1.2/module_assignment.py
@@ -4,13 +4,15 @@
 """
 Exercise 1:
 
-Write a function that searches a list of names (unsorted) for the name "Bob" and returns the location in the list. If Bob is not in the array, return -1.
+Write a function that searches a list of names (unsorted) for the name "Bob" and returns the location 
+in the list. If Bob is not in the array, return -1.
 
-*Example:
+* Example:
     csWhereIsBob(["Jimmy", "Layla", "Bob"]) ➞ 2
 
 Notes:
-    Assume all names start with a capital letter and are lowercase thereafter (i.e. don't worry about finding "BOB" or "bob").
+    Assume all names start with a capital letter and are lowercase thereafter 
+        - (i.e. don't worry about finding "BOB" or "bob")
 """
 
 def csWhereIsBob(names):
@@ -26,3 +28,23 @@ print(csWhereIsBob(["Bob", "Layla", "Kaitlyn", "Patricia"])) # expected: => 0
 print(csWhereIsBob(["Jimmy", "Layla", "James"])) # expected: => -1
 print(csWhereIsBob([""])) # expected: => -1
 print(csWhereIsBob([])) # expected: => -1
+
+
+
+"""
+Exercise 2: 
+
+Imagine a school that children attend for years. In each year, there are a certain number of groups 
+started, marked with the letters. So if years = 7 and groups = 4:
+    - First year the groups are: 1a, 1b, 1c, 1d
+    - Last year the groups are: 7a, 7b, 7c, 7d.
+
+Write a function that returns the groups in the school by year (as a string), separated with a comma and space in the form of "1a, 1b, 1c, 1d, 2a, 2b (....) 6d, 7a, 7b, 7c, 7d".
+
+* Example:
+    csSchoolYearsAndGroups(years = 7, groups = 4) ➞ "1a, 1b, 1c, 1d, 2a, 2b, 2c, 2d, 3a, 3b, 3c, 3d, 4a, 4b, 4c, 4d, 5a, 5b, 5c, 5d, 6a, 6b, 6c, 6d, 7a, 7b, 7c, 7d"
+
+* Constraints:
+    1 <= years <= 10
+    1 <= groups <=26
+"""
