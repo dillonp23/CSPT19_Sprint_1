b6758f9156bf1417e3911f7cac3188c8a1aee626
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-12 23:30:31 -0600

    Added prompt for exercise 1 of CodeSignal assignment

--- 1.2/module_assignment.py
+++ 1.2/module_assignment.py
@@ -1,2 +1,15 @@
 
 # CodeSignal Assignment 1.2
+
+"""
+Exercise 1:
+
+Write a function that searches a list of names (unsorted) for the name "Bob" and returns the location in the list. If Bob is not in the array, return -1.
+
+*Example:
+    csWhereIsBob(["Jimmy", "Layla", "Bob"]) ➞ 2
+
+Notes:
+    Assume all names start with a capital letter and are lowercase thereafter (i.e. don't worry about finding "BOB" or "bob").
+"""
+
