a2b48cbd3248c1cd19860e466a02bb963e6440ab
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-15 21:43:49 -0500

    Added notes on time and space complexity

--- 1.3/lecture_notes.py
+++ 1.3/lecture_notes.py
@@ -2,4 +2,15 @@
 """
 Sprint 1 Module 3: Time & Space Complexity
 
+* Least Time/Space *
+- O(1): Constant
+- O(log n): Logarithmic - moves towards constant as input size grows
+- O(n): Linear - increases proportionally to input size
+- O(n log n)
+- O(n^2): Quadratic 
+- O(c^n): Exponential - increases exponentially as input size increases
+- O(n!): Factorial
+* Most Time/Space *
+
 """
+
