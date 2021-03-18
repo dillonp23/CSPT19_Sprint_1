a05c9b263f9032f170f1c084f5bd919d53f2da25
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-17 20:47:30 -0500

    Added initial lecture notes for binary and how computers represent and handle data

--- 1.4/lecture_notes.py
+++ 1.4/lecture_notes.py
@@ -2,4 +2,43 @@
 """
 # Sprint 1 Module 4 - RAM & Data Structures in Memory
 
+
+* Numbers can be represented in base 2 or base 10
+    - base 2 => binary
+    - base 10 => decimal
+
+
+* "Binary" (base 2)
+    - a binary digit is a bit
+    - 8 bits in a byte
+    - computers are made up of transistors
+    - each transistor is a state of "on" (1/true) or "off" (0/false)
+
+
+* Two main character encodings:
+    1. ASCII
+    - 2^7 or 128 characters
+
+    2. Unicode 
+    - 2^21 characters = ~2.1 million characters
+
+
+Media types in computers:
+    * Images
+        - made up of pixels, which are just combinations of RGB colors represented as numbers
+
+    * Video
+        - similar to images just more data
+
+    * Audio
+        - converts waveforms sampled at different rates to a number frequency
+            - 8-bit or 16-bit
+        
+    * Code in binary (https://indianpythonista.wordpress.com/2018/01/04/how-python-runs/)
+        - an "Interpreter" converts high level language to low level code
+        - "Compiler" => translates .py code to byte code
+        - "Virtual Machine" => translates the byte code to machine code that can be executed by the CPU
 """
+
+
+
