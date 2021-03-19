c62506bcb73941b100c20ec80381f8dc42988963
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-19 15:55:46 -0500

    Added prompt for exercise 6

--- 1.4/module_project.py
+++ 1.4/module_project.py
@@ -23,3 +23,22 @@ possible in an ASCII set?
 Answer: each bit can be either a one or zero, therefore ASCII representation with 7 bits would be:
         2^7 = 128 charcaters
 """
+
+
+"""
+Exercise 6
+
+Given an integer, write a function that reverses the bits (in binary) and returns the integer result.
+
+* Notes:
+    The input integer will not be negative.
+
+* Examples:
+    csReverseIntegerBits(417) -> 267
+    417 in binary is 110100001. Reversing the binary is 100001011, which is 267 in decimal.
+    csReverseIntegerBits(267) -> 417
+    csReverseIntegerBits(0) -> 0
+"""
+
+def csReverseIntegerBits(n):
+    pass
