16dcbf25b4146ac0c39ec8f6751d358bd0baa2a0
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-18 23:02:49 -0500

    Added prompt and solution to exercise #5

--- 1.4/module_project.py
+++ 1.4/module_project.py
@@ -11,3 +11,15 @@ Convert 78 to binary, then add the resulting digits in decimal
 
 Answer: 78 -> 1001110 -> sum: 1+1+1+1 = 4
 """
+
+
+"""
+Exercise 5:
+
+In order to store strings, computers encode with ASCII or Unicode. In ASCII, each character can be represented
+by 7 bits (although commonly stored as 8 bits). With this knowledge, what is the maximum number of characters
+possible in an ASCII set?
+
+Answer: each bit can be either a one or zero, therefore ASCII representation with 7 bits would be:
+        2^7 = 128 charcaters
+"""
