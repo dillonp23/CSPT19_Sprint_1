5ae1fac4506d061221d41d127b41c281f51eb919
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-18 22:17:06 -0500

    Added solution to exercise 3 of module project

---
+++ 1.4/module_project.py
@@ -0,0 +1,13 @@
+
+
+# Sprint 1 - Module 4 Project
+
+"""
+Exercise 3: 
+
+Convert 78 to binary, then add the resulting digits in decimal
+
+78 -> binary -> sum of digits
+
+Answer: 78 -> 1001110 -> sum: 1+1+1+1 = 4
+"""
