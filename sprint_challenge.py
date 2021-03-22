7dec553d66d66709122b6882459b0c93c7954633
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-21 22:10:24 -0500

    Added solution to exercise 3

--- sprint_challenge.py
+++ sprint_challenge.py
@@ -96,3 +96,28 @@ only contain the first occurrence of each word in the string.
     csRemoveDuplicateWords("alpha bravo bravo golf golf golf delta alpha bravo bravo golf golf golf delta") -> "alpha bravo golf delta"
     csRemoveDuplicateWords("my dog is my dog is super smart") -> "my dog is super smart"
 """
+
+def csRemoveDuplicateWords(input_str):
+    word_list = []
+    word = ""
+    length = len(input_str) - 1
+    
+    for i in range(len(input_str)):
+        char = input_str[i]
+        
+        if char.isalpha():
+            word += char
+        
+        if i == length or char == " ":
+            if word not in word_list:
+                word_list.append(word)
+            
+            word = ""         
+    
+    
+    return " ".join(word_list)
+
+
+print(csRemoveDuplicateWords("alpha bravo bravo golf golf golf delta alpha bravo bravo golf golf golf delta"))
+print(csRemoveDuplicateWords("my dog is my dog is super smart"))
+print(csRemoveDuplicateWords("this is only a test string test"))
