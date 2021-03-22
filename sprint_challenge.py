16d0cb8f742936d3fa7ff4ed581d4262f7f644d6
Dillon P <52013691+dillonp23@users.noreply.github.com>
2021-03-21 21:21:22 -0500

    Added prompt for exercise 3 of sprint challenge

--- sprint_challenge.py
+++ sprint_challenge.py
@@ -82,3 +82,17 @@ print(csCheckPalindrome("anna"))
 print(csCheckPalindrome("12345"))
 print(csCheckPalindrome("12321"))
 print(csCheckPalindrome("teststring"))
+
+
+
+
+"""
+Exercise 3
+
+Given a string, write a function that removes all duplicate words from the input. The string that you return should 
+only contain the first occurrence of each word in the string.
+
+* Examples:
+    csRemoveDuplicateWords("alpha bravo bravo golf golf golf delta alpha bravo bravo golf golf golf delta") -> "alpha bravo golf delta"
+    csRemoveDuplicateWords("my dog is my dog is super smart") -> "my dog is super smart"
+"""
